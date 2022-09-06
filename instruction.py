127eba01bcf75f5fd3cd84d3d65087bfbf34015a
Cooper <maxime@fuzzinglabs.com>
2022-09-06 15:52:32 +0200

    [Function] Added handling for opcode imm imm into reg

--- instruction.py
+++ instruction.py
@@ -1,4 +1,3 @@
-from dis import Bytecode
 from enum import Enum, auto
 
 
@@ -60,29 +59,92 @@ class opcode(Enum):
     Ternary = auto()
     Xor = auto()
 
+class Operand(Enum):
+    Literal = 0
+    Register = 1
+    ProgramID = 2
+    Caller = 3
+
+class Literal(Enum):
+    address = 0,
+    boolean = 1
+    field = 2
+    group = 3
+    i8 = 4
+    i16 = 5
+    i32 = 6
+    i64 = 7
+    i128 = 8
+    u8 = 9
+    u16 = 10
+    u32 = 11
+    u64 = 12
+    u128 = 13
+    scalar = 14
+    string = 15
+
 
 class instruction:
     def __init__(self) -> None:
         self.bytecodes = None
 
-    def read_function_regin_regin_regout(self, opcode_name):
+    def read_literal(self, type):
+        res = "UNHANDLED LITERAL TYPE"
+        if type == Literal.i128 or type == Literal.u128:
+            res = int.from_bytes(self.bytecodes[:16], "little")
+            self.bytecodes = self.bytecodes[16:]
+                                                        
+        elif type == Literal.i64 or type == Literal.u64:
+            res = int.from_bytes(self.bytecodes[:8], "little")
+            self.bytecodes = self.bytecodes[8:]
+
+                
+        elif type == Literal.i32 or type == Literal.u32:
+            res = int.from_bytes(self.bytecodes[:4], "little")
+            self.bytecodes = self.bytecodes[4:]
+
+        elif type == Literal.i16 or type == Literal.u16:
+            res = int.from_bytes(self.bytecodes[:2], "little")
+            self.bytecodes = self.bytecodes[2:]
+        
+        elif type == Literal.i8 or type == Literal.u8:
+            res = int.from_bytes(self.bytecodes[:1], "little")
+            self.bytecodes = self.bytecodes[1:]
+        
+        return res
+
+
+    def read_function_in_in_regout(self, opcode_name):
         operands = []
 
+        print(self.bytecodes)
+
         for _ in range(2):
-            op_type = int.from_bytes(self.bytecodes[:2], "little")
-            self.bytecodes = self.bytecodes[2:]
-            if op_type != 1:
-                print(f"TODO opcode handling for opcode: {opcode_name}")
-            else:
-                operands.append(self.bytecodes[0])
+            op_type = Operand(int.from_bytes(self.bytecodes[:1], "little"))
+            self.bytecodes = self.bytecodes[1:]
+            if op_type == Operand.Literal:
+                literal_type = Literal(int.from_bytes(self.bytecodes[:2], "little"))
+                self.bytecodes = self.bytecodes[2:]
+                value = self.read_literal(literal_type)
+                operands.append("{}{}".format(value, literal_type.name))
+
+
+
+            elif op_type == Operand.Register:
+                self.bytecodes = self.bytecodes[1:] # Skip an unsed byte for registers
+                register = int.from_bytes(self.bytecodes[:1], "little")
+                operands.append("r{}".format(register))
                 self.bytecodes = self.bytecodes[1:]
+
+
         unk = self.bytecodes[0]
         self.bytecodes = self.bytecodes[1:]
 
-        operands.append(self.bytecodes[0])
+        register = int.from_bytes(self.bytecodes[:1], "little")
+        operands.append("r" + str(register))
         self.bytecodes = self.bytecodes[1:]
         print(
-            f"{opcode_name} r{operands[0]} r{operands[1]} into r{operands[2]}"
+            f"{opcode_name} {operands[0]} {operands[1]} into {operands[2]}"
         )
 
     def read_function_instructions(self):
@@ -91,7 +153,7 @@ class instruction:
         opcode_name = opcode(index).name
         
         # Need to make lists of function using the same pattern as xor (input1, input2, output) to dont decompile wrongly
-        self.read_function_regin_regin_regout(opcode_name)
+        self.read_function_in_in_regout(opcode_name)
 
 
     def disassemble_instruction(self, bytes):
