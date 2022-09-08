3e4af9cd71323847d07308530d5e475c8a0c4149
Nabih <nabih@fuzzinglabs.com>
2022-09-08 17:24:43 +0200

    [tests] update testsuit

--- tests/test.py
+++ tests/test.py
@@ -6,6 +6,8 @@ from difflib import SequenceMatcher
 def build_tests():
     cwd = os.getcwd()
     subfolders = [f.path for f in os.scandir("tests") if f.is_dir()]
+    crash = 0
+    total = 0
     for folder in subfolders:
         os.chdir(folder)
         subprocess.run("aleo build", shell=True, check=True)
@@ -18,16 +20,21 @@ def build_tests():
             content_file = f.readlines()
             content_file = "".join(content_file)
             f.close()
-            output, err = p.communicate()
+            output, _ = p.communicate()
             output = output.decode("utf-8")
             # print(output)
             # print("----")
             # print(content_file)
             ratio = SequenceMatcher(None, output, content_file).ratio()
             print(ratio)
+            total += ratio
         else:
+            crash += 1
             print("NO BUILD DIRECTORY")
         os.chdir(cwd)
+    print("-------------------------------------------------")
+    print(f"CRASH TOTAL == {crash}")
+    print(f"RATIO -- {total/len(subfolders)}")
 
 
 build_tests()
