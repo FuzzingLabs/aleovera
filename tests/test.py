e9954893fb0f88691ef74f563516537a5582ec60
Cooper <maxime@fuzzinglabs.com>
2022-09-09 14:16:12 +0200

    [tests] Removing trailing whitespaces and comments in code files

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import subprocess
 from difflib import SequenceMatcher
 
@@ -9,8 +10,8 @@ def build_tests():
     crash = 0
     total = 0
     for folder in subfolders:
-        print("-------------------")
-        print(folder)
+        print("-------------------", file=sys.stderr)
+        print(folder, file=sys.stderr)
         os.chdir(folder)
         subprocess.check_call(
             "aleo build", shell=True, stdout=subprocess.DEVNULL
@@ -22,8 +23,14 @@ def build_tests():
                 stdout=subprocess.PIPE,
             )
             f = open("main.aleo")
-            content_file = f.readlines()
-            content_file = "".join(content_file)
+            unparsed_lines = f.readlines()
+            file_lines = []
+            for line in unparsed_lines:
+                if line[:2] == "//":
+                    continue
+                file_lines.append(line.rstrip() + "\n")
+                
+            content_file = "".join(file_lines)
             f.close()
             output, _ = p.communicate()
             output = output.decode("utf-8")
@@ -31,15 +38,32 @@ def build_tests():
             # print("----")
             # print(content_file)
             ratio = SequenceMatcher(None, output, content_file).ratio()
-            print(ratio)
+            if ratio != 1 and len(sys.argv) == 2 and sys.argv[1] == "-d":
+                output_lines = output.split("\n")
+                outlen = len(output_lines)
+                fillen = len(file_lines)
+                i = 0
+                while (i < outlen and i < fillen):
+                    if file_lines[i] != output_lines[i]:
+                        print(f">>> {file_lines[i]}")
+                        print(f"<<< {output_lines[i]}")
+                    i += 1
+                while (i < outlen):
+                    print(f"<<< {output_lines[i]}")
+                    i += 1
+                while (i < fillen):
+                    print(f">>> {file_lines[i]}")
+                    i+= 1
+
+            print(ratio, file=sys.stderr)
             total += ratio
         else:
             crash += 1
-            print("NO BUILD DIRECTORY")
+            print("NO BUILD DIRECTORY", file=sys.stderr)
         os.chdir(cwd)
-    print("-------------------------------------------------")
-    print(f"Folder that does not build : {crash}")
-    print(f"RATIO -- {total/(len(subfolders) - crash)}")
+    print("-------------------------------------------------", file=sys.stderr)
+    print(f"Folder that does not build : {crash}", file=sys.stderr)
+    print(f"RATIO -- {total/(len(subfolders) - crash)}", file=sys.stderr)
 
 
 build_tests()
