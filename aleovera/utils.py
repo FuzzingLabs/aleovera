51fca59172cef47d5a0181327db099086f4a9006
Nabih <nabih@fuzzinglabs.com>
2022-09-12 16:48:56 +0200

    [utils][finalize] fix xexit

--- aleovera/utils.py
+++ aleovera/utils.py
@@ -88,7 +88,7 @@ def xprint(*args, end="\n"):
 
 
 def xexit(error=""):
-    if error != "":
+    if error == "":
         exc_type, _, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         print(exc_type, fname, exc_tb.tb_lineno)
